0fa83c7b08823f0fd4cfeb9eb92dabd250b1ae13
Yaswanthv5 <89481859+Yaswanthv5@users.noreply.github.com>
2025-02-01 19:49:59 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const path = require('path');
-
-module.exports = {
-  webpack: (config) => {
-    config.resolve.alias['@']=path.join(__dirname,'.');
-    return config;
-  },
-};
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
